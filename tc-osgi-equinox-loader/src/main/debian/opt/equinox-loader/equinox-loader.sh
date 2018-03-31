9096547dcbf38b1bbde545ad6dbc5e6c73dfc4a8
Thomas Collonville <collonville_tom@yahoo.fr>
2018-03-31 19:41:34 +0200

    modif des scirpt de demarrage

--- tc-osgi-equinox-loader/src/main/debian/opt/equinox-loader/equinox-loader.sh
+++ tc-osgi-equinox-loader/src/main/debian/opt/equinox-loader/equinox-loader.sh
@@ -9,54 +9,110 @@
 # set java home variable if not define in environnemt 
 # JAVA_HOME=
 
-USER_PARAM="$1"
-JAVA_EXE="java"
+buildHelp()
+{
+	echo "aide:
+		--user permet de specifier un autre utilisateur
+		--debug active le debug osgi
+		--console active la console
+		--jmx active le connecteur jmx
+    --xm permet de modifier les parametres java
+	"
+}
+
+
+#$1 user
+#$2 cmd
+launch()
+{
+  if [ -n "$JAVA_HOME" ]
+  then
+    echo "LOG: sudo -u $1 $JAVA_HOME/bin/$2"
+    bash -c "sudo -u $1  $JAVA_HOME/bin/$2"
+  else
+    echo "JAVA_HOME not set, please intall java jre, trying to use java cmd"
+    echo "LOG: sudo -u $1 $2"
+    bash -c "sudo -u $1 $2"
+  fi
+}
+
+#$1 user
+cleanConfDir()
+{
+	bash -c "sudo -u $1 rm -fr $CONFIG_DIR/org.eclipse.osgi"
+	echo "LOG: sudo -u $1 rm -r $CONFIG_DIR/org.eclipse.osgi"
+}
+
+
 
 
 INSTALL_DIR="/opt/equinox-loader"
+USER_PARAM="equinox-loader"
+CONFIG_DIR="$INSTALL_DIR/bundles/configuration"
+OPT_ARGS=""
+JMX_PORT=9010
+JMX=""
+XM="-Xmx512m -XX:MaxPermSize=64m"
 
-if [ -z "$USER_PARAM" ];
+while [ -n "$1" ]
+do
+  case $1 in
+    --user)    USER_PARAM="$2";shift;;
+    --debug)   OPT_ARGS="$OPT_ARGS -debug";shift;;
+	  --console) OPT_ARGS="$OPT_ARGS -console";shift;;
+    --jmx)     JMX="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -DJEMonitor=true";shift;;
+    --xm)	     XM="$2";shift;;
+    -h)        HELP="true";buildHelp;;
+    --help)    HELP="true";buildHelp;;
+    *)        ;;
+  esac
+  shift
+done
+
+
+HERE=$(pwd)
+cd $INSTALL_DIR
+
+#on regarde sous quel utilisateur on va lance equinox-loader si non specifier on le lance sous equinox-loader
+if [ "$USER_PARAM" != "equinox-loader" ];
 then
-  USER_PARAM="equinox-loader"
-  CONFIG_DIR="$INSTALL_DIR/bundles/configuration"
-else
   CONFIG_DIR="$HOME/equinox-loader/bundles/configuration"
 fi
 
 
-if [ "$OSTYPE" == "cygwin" ] 
-then
-  PRIVILEGE=""
-  INSTALL_DIR="/opt/equinox-loader"
-  CONFIG_DIR="$INSTALL_DIR/bundles/configuration"
-  JAVA_EXE="$JAVA_EXE.exe"
-else
-  PRIVILEGE="sudo -u $USER_PARAM"	
-  
-fi 
-
 ### Options -os ${target.os} -ws ${target.ws} -arch ${target.arch} -nl ${target.nl} -consoleLog
 # -os win32 -ws win32 -arch x86_64 -nl fr_FR -consoleLog
-# 
-EQUINOX_OPTS="-configuration $CONFIG_DIR -console -debug"
-JAVA_OPTS="-Xmx512m -XX:MaxPermSize=64m -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -DJEMonitor=true"
+
+EQUINOX_OPTS="-configuration $CONFIG_DIR $OPT_ARGS"
+JAVA_OPTS="$XM $JMX"
 
 #Commande line
-CMD="$JAVA_EXE $JAVA_OPTS -jar ${project.artifactId}-${project.version}-assembly.jar $EQUINOX_OPTS"
-
-# execution
-pushd $INSTALL_DIR
-if [ -n "$JAVA_HOME" ]
-then
-  echo "LOG: $PRIVILEGE $JAVA_HOME/bin/$CMD"
-  bash -c "$PRIVILEGE  $JAVA_HOME/bin/$CMD"
-else
-  echo "JAVA_HOME not set, please intall java jre, trying to use java cmd"
-  echo "LOG: $PRIVILEGE $CMD"
-  bash -c "$PRIVILEGE $CMD"
-fi
+CMD="java $JAVA_OPTS -jar ${project.artifactId}-${project.version}-assembly.jar $EQUINOX_OPTS"
 
 
-bash -c "$PRIVILEGE rm -fr $CONFIG_DIR/org.eclipse.osgi"
-echo "LOG: $PRIVILEGE rm -r $CONFIG_DIR/org.eclipse.osgi"
-popd
+launch "$USER_PARAM" "$CMD"
+cleanConfDir "$USER_PARAM"
+
+
+cd $HERE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
