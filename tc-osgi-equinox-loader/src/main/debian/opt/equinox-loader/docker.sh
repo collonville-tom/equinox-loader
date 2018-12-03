e7caf373140663d852a0707f3084a72891deb199
Thomas Collonville <collonville.thomas@gmail.com>
2018-12-03 22:19:19 +0100

    ajout script pour docker + projet manager

---
+++ tc-osgi-equinox-loader/src/main/debian/opt/equinox-loader/docker.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+cd /opt/equinox-loader
+java -Xmn128m -Xss128m -Xmx512m -XX:MaxMetaspaceSize=64m -verbose:gc -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7001 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -DJEMonitor=true -jar ${project.artifactId}-${project.version}-assembly.jar -configuration /var/equinox-loader/bundles/configuration -debug -console
