75f99cf5a24b60b5e131a2a1299c0ab37188198d
Thomas Collonville <collonville.thomas@gmail.com>
2019-01-02 22:26:58 +0100

    maj pour  utilisation jmx

--- tc-osgi-equinox-loader/src/main/debian/opt/equinox-loader/docker.sh
+++ tc-osgi-equinox-loader/src/main/debian/opt/equinox-loader/docker.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-
 cd /opt/equinox-loader
-java -Xmn128m -Xss128m -Xmx512m -XX:MaxMetaspaceSize=64m -verbose:gc -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=${jmx.port} -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -DJEMonitor=true -jar ${project.artifactId}-${project.version}-assembly.jar -configuration /var/equinox-loader/bundles/configuration -debug -console
+java -Xmn128m -Xss128m -Xmx512m -XX:MaxMetaspaceSize=64m -verbose:gc \
+     -Dcom.sun.management.jmxremote \
+     -Djava.rmi.server.hostname=$DOCKER_HOST_IP \
+     -Dcom.sun.management.jmxremote.port=${jmx.port} \
+     -Dcom.sun.management.jmxremote.rmi.port=${jmx.port} \
+     -Dcom.sun.management.jmxremote.local.only=false \
+     -Dcom.sun.management.jmxremote.authenticate=false \
+     -Dcom.sun.management.jmxremote.ssl=false \
+     -DJEMonitor=true \
+     -jar ${project.artifactId}-${project.version}-assembly.jar \
+     -configuration /var/equinox-loader/bundles/configuration -debug -console
+     
+# pour utiliser l'authentification par mot de passe il faut initialiser le fichier password du repertoire management du jre
